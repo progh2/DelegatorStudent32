110f7ea932dc05c1e06dca6c0d3ac28e9be9ba41
Gihun Ham <ham@e-mirim.hs.kr>
2023-08-29 13:43:16 +0900

    델리게이터를 활용한 다양한 형태로의 학생 명단 출력 구현

--- DelegatorStudent32/Program.cs
+++ DelegatorStudent32/Program.cs
@@ -10,6 +10,25 @@ namespace DelegatorStudent32
     {
         static void Main(string[] args)
         {
+            Students students = new Students();
+            students.Add(new Student("함기훈", 4.5));
+            students.Add(new Student("박성래", 4.4));
+            students.Add(new Student("마이콜", 1.0));
+
+            students.Print();
+            students.Print((s) =>
+            {
+                Console.WriteLine("*************");
+                Console.WriteLine(" 이름:" + s.Name);
+                Console.WriteLine(" 학점:" + s.Score);
+            });
+            students.Print((s) =>
+            {
+                Console.WriteLine("<student>");
+                Console.WriteLine("  <name>"+ s.Name + "</name>");
+                Console.WriteLine("  <score>" + s.Score + "</score>");
+                Console.WriteLine("</student>");
+            });
         }
     }
 }
